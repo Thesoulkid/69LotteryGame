0abbbb1f64aee0915298842d06693675dc0d81d9
Thesoulkid <133838463+Thesoulkid@users.noreply.github.com>
2025-01-31 23:47:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,11 @@
-let balance = 1000;
-let users = JSON.parse(localStorage.getItem('users')) || [];
-
-function login() {
-    let username = document.getElementById("username").value;
-    let loginTime = new Date().toLocaleString();
-
-    users.push({ username, loginTime });
-    localStorage.setItem("users", JSON.stringify(users));
-
-    window.location.href = "home.html";
-}
-
-function startGame(duration) {
-    let timeLeft = duration;
-    document.getElementById("timer").innerText = timeLeft;
-    
-    let timer = setInterval(() => {
-        timeLeft--;
-        document.getElementById("timer").innerText = timeLeft;
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            generateResult();
-        }
-    }, 1000);
-}
-
-function generateResult() {
-    let randomNumber = Math.floor(Math.random() * 10);
-    let result = (randomNumber < 5) ? "small" : "big";
-    document.getElementById("result").innerText = Number: ${randomNumber} - ${result.toUpperCase()};
-}
-
-function generateQR() {
-    let amount = document.getElementById("amount").value;
-    if (amount < 100 || amount > 50000) {
-        alert("Enter amount between ₹100 - ₹50,000");
-        return;
-    }
-
-    let qrcode = new QRCode(document.getElementById("qrcode"), {
-        text: upi://pay?pa=6238271749@fam&pn=69Lottery&mc=0000&tid=1234567890&tr=${amount}&tn=Deposit&am=${amount}&cu=INR,
-        width: 150,
-        height: 150
-    });
-}
+function moveRandomEl(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  }
+  
+  const moveRandom = document.querySelector("#move-random");
+  
+  moveRandom.addEventListener("mouseenter", function (e) {
+    moveRandomEl(e.target);
+  });
